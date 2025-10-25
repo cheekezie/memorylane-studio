498707b5de757b490e1fb6609db1fb383fabbe41
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2025-10-25 15:15:01 +0100

    refac: update component

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -9,32 +9,50 @@ interface RootLayoutProps {
 
 const RootLayout = ({ children }: RootLayoutProps) => {
 	const [sidebarOpen, setSidebarOpen] = useState(false);
+	const [activeMenuItem, setActiveMenuItem] = useState("frame-photo");
 
 	return (
-		<div className="flex h-screen overflow-hidden">
-			<aside className="hidden lg:block lg:w-64 bg-primary flex-shrink-0">
-				<Sidebar />
+		<div className="flex h-screen bg-gray-50 overflow-hidden">
+			{/* Desktop */}
+			<aside className="hidden lg:block lg:w-64 m-4">
+				<Sidebar
+					isOpen
+					activeItem={activeMenuItem}
+					onItemSelect={setActiveMenuItem}
+				/>
 			</aside>
 
+			{/* Mobile overlay */}
 			{sidebarOpen && (
 				<div
-					className="fixed inset-0 bg-black/50 z-40 lg:hidden"
+					className="fixed inset-0 bg-black/40 z-40 lg:hidden"
 					onClick={() => setSidebarOpen(false)}
 				/>
 			)}
 
+			{/* Mobile Sidebar */}
 			<aside
-				className={`fixed top-0 left-0 h-full w-64 bg-primary z-50 transform transition-transform duration-300 lg:hidden ${
+				className={`fixed top-0 left-0 h-full w-64 z-50 transform transition-transform duration-300 lg:hidden ${
 					sidebarOpen ? "translate-x-0" : "-translate-x-full"
 				}`}
 			>
-				<Sidebar onClose={() => setSidebarOpen(false)} />
+				<Sidebar
+					isOpen={sidebarOpen}
+					activeItem={activeMenuItem}
+					onItemSelect={setActiveMenuItem}
+					onClose={() => setSidebarOpen(false)}
+				/>
 			</aside>
 
-			<div className="flex-1 flex flex-col overflow-hidden">
-				<Header onMenuClick={() => setSidebarOpen(true)} />
+			<div className="flex flex-col flex-1 overflow-hidden">
+				<div className="m-4">
+					<Header
+						onMenuClick={() => setSidebarOpen(true)}
+						activeMenuItem={activeMenuItem}
+					/>
+				</div>
 
-				<main className="flex-1 overflow-y-auto bg-gray-50">{children}</main>
+				<main className="flex-1 overflow-y-auto px-4 pb-6">{children}</main>
 			</div>
 		</div>
 	);
