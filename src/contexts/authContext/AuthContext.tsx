a0d1bf8bc59577bd0adaad3ce8d4d3e48bb04899
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2025-10-28 11:53:15 +0100

    refac: update component

--- src/contexts/authContext/AuthContext.tsx
+++ src/contexts/authContext/AuthContext.tsx
@@ -1,11 +1,10 @@
 import { createContext } from "react";
 
-interface AuthContextType {
+export interface AuthContextType {
 	isAuthenticated: boolean;
-	setIsAuthenticated: (isAuth: boolean) => void;
+	setIsAuthenticated: (value: boolean) => void;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(
 	undefined,
 );
-export type { AuthContextType };
