ca00a0c29019a23065613b81915109ef3a9fe9a7
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2025-10-28 11:52:55 +0100

    feat: add db for location

---
+++ src/db/Location.ts
@@ -0,0 +1,42 @@
+//Just generated list -- will request from Harry
+
+export const LOCATIONS = {
+	"United Kingdom": {
+		cities: [
+			"London",
+			"Manchester",
+			"Birmingham",
+			"Liverpool",
+			"Edinburgh",
+			"Glasgow",
+			"Bristol",
+			"Leeds",
+			"Sheffield",
+			"Cardiff",
+			"Belfast",
+			"Newcastle",
+			"Nottingham",
+			"Southampton",
+			"Leicester",
+		],
+	},
+	Nigeria: {
+		cities: [
+			"Lagos",
+			"Abuja",
+			"Kano",
+			"Ibadan",
+			"Port Harcourt",
+			"Benin City",
+			"Kaduna",
+			"Enugu",
+			"Aba",
+			"Ilorin",
+			"Jos",
+			"Warri",
+			"Calabar",
+			"Owerri",
+			"Abeokuta",
+		],
+	},
+};
