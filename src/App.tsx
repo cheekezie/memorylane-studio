0570e0faa7cccef3491c1b2b8137a07ecb9227d6
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2025-10-28 11:50:53 +0100

    feat: add infoModal

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,35 @@ import RootLayout from "./layout/RootLayout";
 import { privateRoutes, publicRoutes } from "./routes";
 import { AppInitializer } from "./providers/AppInitializer";
 import "@ant-design/v5-patch-for-react-19";
-
 function App() {
 	return (
 		<AppInitializer>
+			{" "}
 			<RootLayout>
+				{" "}
 				<Suspense fallback={<Loader text="Loading..." theme="white" />}>
+					{" "}
 					<Routes>
+						{" "}
 						{publicRoutes.map((route) => (
 							<Route
 								key={route.path}
 								path={route.path}
 								element={<route.element />}
 							/>
-						))}
-
+						))}{" "}
 						{privateRoutes.map((route) => (
 							<Route
 								key={route.path}
 								path={route.path}
 								element={<route.element />}
 							/>
-						))}
-
-						<Route path="*" element={<Navigate to="/" replace />} />
-					</Routes>
-				</Suspense>
-			</RootLayout>
+						))}{" "}
+						<Route path="*" element={<Navigate to="/" replace />} />{" "}
+					</Routes>{" "}
+				</Suspense>{" "}
+			</RootLayout>{" "}
 		</AppInitializer>
 	);
 }
-
 export default App;
