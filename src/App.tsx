a1a45b2b185d4f655ecb090ade84d060e6e3cdc8
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2025-11-06 14:26:21 +0100

    refac: update component

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,64 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import { Suspense } from "react";
+import { Suspense, createElement, isValidElement } from "react";
 import Loader from "./components/Loader";
 import RootLayout from "./layout/RootLayout";
 import { privateRoutes, publicRoutes } from "./routes";
 import { AppInitializer } from "./providers/AppInitializer";
 import "@ant-design/v5-patch-for-react-19";
+
+function renderElement(el: any) {
+	if (isValidElement(el)) return el;
+	return createElement(el);
+}
+
 function App() {
 	return (
 		<AppInitializer>
-			{" "}
 			<RootLayout>
-				{" "}
 				<Suspense fallback={<Loader text="Loading..." theme="white" />}>
-					{" "}
 					<Routes>
-						{" "}
-						{publicRoutes.map((route) => (
-							<Route
-								key={route.path}
-								path={route.path}
-								element={<route.element />}
-							/>
-						))}{" "}
+						{publicRoutes.map((route) => {
+							if (route.children) {
+								return (
+									<Route
+										key={route.path}
+										path={route.path}
+										element={renderElement(route.element)}
+									>
+										{route.children.map((child) => (
+											<Route
+												key={child.path}
+												path={child.path}
+												element={renderElement(child.element)}
+											/>
+										))}
+									</Route>
+								);
+							}
+
+							return (
+								<Route
+									key={route.path}
+									path={route.path}
+									element={renderElement(route.element)}
+								/>
+							);
+						})}
+
 						{privateRoutes.map((route) => (
 							<Route
 								key={route.path}
 								path={route.path}
-								element={<route.element />}
+								element={renderElement(route.element)}
 							/>
-						))}{" "}
-						<Route path="*" element={<Navigate to="/" replace />} />{" "}
-					</Routes>{" "}
-				</Suspense>{" "}
-			</RootLayout>{" "}
+						))}
+
+						<Route path="*" element={<Navigate to="/" replace />} />
+					</Routes>
+				</Suspense>
+			</RootLayout>
 		</AppInitializer>
 	);
 }
+
 export default App;
