e0e18bc19b72bbfb825b2fa5c7eeeaf7be88867b
Anthony Ikwuegbu <ikwuegbuanthony@gmail.com>
2025-11-18 08:37:51 +0100

    refac: update component

--- src/constants/cartBreadcrumbSteps.ts
+++ src/constants/cartBreadcrumbSteps.ts
@@ -2,5 +2,5 @@ import type { BreadcrumbStep } from "../components/BreadCrumb";
 
 export const CART_CHECKOUT_STEPS: BreadcrumbStep[] = [
 	{ id: "cart", label: "My Cart", path: "/cart" },
-	{ id: "checkout", label: "Check Out", path: "/checkout" },
+	{ id: "checkout", label: "Check Out", path: "/cart/checkout" },
 ];
